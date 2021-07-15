d1d0268964086834c7c43b5b01dade1493d47bb7
sam parker <hisamparker@gmail.com>
2021-07-15 10:32:13 +0200

    update serverjs

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -9,6 +9,11 @@ app.get('/', (req, res) => {
     res.json({ msg: "oh hai there"})
 })
 
+//define and require routes
+app.use('/api/users', require('./routes/users'))
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/contacts', require('./routes/contacts'))
+
 //tell express to listen with the listen method and give it a port var from our .env
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`the walls have ears, the hills have eyes ${PORT}`))
